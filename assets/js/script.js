1ebead194ac581b6a4ed01cfac4cda504b9f2b18
Jay SinnaduraI <jay.sinnadurai@gmail.com>
2024-07-25 12:31:28 +0000

    Add html buttons and css styles

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let quizQuestions = [
+    {
+        question: 'When was the first modern Olympic Games held?',
+        options: ['1689', '1896', '1972'],
+        answer: '1896'
+    },
+    {
+        question: 'In which city did the Olympic Games originate?',
+        options: ['Paris', 'Tokyo', 'Athens'],
+        answer: 'Athens'
+    },
+    {
+        question: 'Which Olympic medal is awarded to competitiors who finish in 2nd place?',
+        options: ['Gold', 'Silver', 'Bronze'],
+        answer: 'Silver'
+    }
+]
+
